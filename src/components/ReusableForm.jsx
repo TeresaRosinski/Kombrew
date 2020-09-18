45f630a0f55b488b214d5d12787d4105c84320a6
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2020-09-18 18:14:21 -0500

    add reusable form

---
+++ src/components/ReusableForm.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function ReusableForm(props) {
+  return (
+    <React.Fragment>
+      <form onSubmit={[props.formSubmissionHandler]}>
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <label>Kombucha Name</label>
+              </td>
+              <td>
+                <input type="text" name="name" placeholder="Kombucha Name" />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label>Kombucha Brand</label>
+              </td>
+              <td>
+                <input type="text" name="brand" placeholder="Kombucha Brand" />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label>Flavor</label>
+              </td>
+              <td>
+                <input
+                  type="text"
+                  name="flavor"
+                  placeholder="Kombucha Flavor"
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label>Price</label>
+              </td>
+              <td>
+                <input
+                  type="number"
+                  name="price"
+                  placeholder="Price Per Pint"
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label>Alcohol Content</label>
+              </td>
+              <td>
+                <input
+                  type="number"
+                  name="alcoholContent"
+                  placeholder="Alcohol Content"
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <button type="submit">{props.buttonText}</button>
+      </form>
+    </React.Fragment>
+  );
+}
